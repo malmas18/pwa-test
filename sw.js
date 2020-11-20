197a7cd7375b997f9c47fafea0121e53ac62d192
malmas18 <29763502+malmas18@users.noreply.github.com>
2020-11-20 01:07:20 +0000

    Updates

--- sw.js
+++ sw.js
@@ -172,11 +172,41 @@ workbox.routing.registerRoute(/\/.gatsby-plugin-offline:.+/, handleAPIRequest)
 
 // show a notification after 15 seconds (the notification
 // permission must be granted first)
-setTimeout(() => {
-    self.registration.showNotification("Hello, world!")
-}, 15000);
+// setTimeout(() => {
+//     self.registration.showNotification("Hello, world!")
+// }, 15000);
 // register a custom navigation route
 // const customRoute = new workbox.routing.NavigationRoute(({ event }) => {
 //     // ...
 //   })
 //   workbox.routing.registerRoute(customRoute)
+
+function notification(text) {
+    self.registration.showNotification(text)
+}
+
+function displayNotification() {
+    //Ask user if we show notifications
+    if (window.Notification && Notification.permission === 'granted') {
+      notification("Hello, world!");
+      // We will create this function in a further step.
+    }
+    // If the user hasn't told whether he wants to be notified or not
+    // Note: because of Chrome, we cannot be sure the permission property
+    // is set, therefore it's unsafe to check for the "default" value.
+    else if (window.Notification && Notification.permission !== 'denied') {
+      Notification.requestPermission(status => {
+        if (status === 'granted') {
+            notification("Hello, world!");
+        } else {
+          alert('You denied or dismissed permissions to notifications.');
+        }
+      });
+    } else {
+      // If the user refuses to get notified
+      alert(
+        'You denied permissions to notifications. Please go to your browser or phone setting to allow notifications.'
+      );
+    }
+  }
+  displayNotification();
