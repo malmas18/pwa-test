615dce751f22de2f47bcd096659453603f6babca
malmas18 <29763502+malmas18@users.noreply.github.com>
2020-12-10 21:21:07 -0800

    add

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-(function() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-     .register('sw.js', { scope: './pwa-test'})
-     .then(function() { 
-        console.log('Service Worker Registered'); 
-      });
-  }
-})();
+// (function() {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//      .register('sw.js', { scope: './pwa-test'})
+//      .then(function() { 
+//         console.log('Service Worker Registered'); 
+//       });
+//   }
+// })();
+
+import {Workbox} from 'workbox-window';
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js', { scope: '/pwa-test/'});
+
+  wb.register();
+}
