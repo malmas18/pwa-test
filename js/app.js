91c212c941abf1368a8f538fb2638ecb59711ad2
malmas18 <29763502+malmas18@users.noreply.github.com>
2020-12-10 21:25:20 -0800

    fix import

--- js/app.js
+++ js/app.js
@@ -7,12 +7,3 @@
 //       });
 //   }
 // })();
-
-import {Workbox} from 'https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-window.prod.mjs';
-
-
-if ('serviceWorker' in navigator) {
-  const wb = new Workbox('/sw.js', { scope: '/pwa-test/'});
-
-  wb.register();
-}
