494cd477a0e831480ffa53f8fa79cf7839fafcaa
malmas18 <29763502+malmas18@users.noreply.github.com>
2020-12-10 21:35:35 -0800

    test

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-// (function() {
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//      .register('sw.js', { scope: './pwa-test'})
-//      .then(function() { 
-//         console.log('Service Worker Registered'); 
-//       });
-//   }
-// })();
+(function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+     .register('sw.js', { scope: '/pwa-test'})
+     .then(function() { 
+        console.log('Service Worker Registered'); 
+      });
+  }
+})();
